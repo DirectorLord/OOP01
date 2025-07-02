9064ae2129f100dcea3d3db4dea75b2e05a7c555
DirectorLord <thelord161203@gmail.com>
2025-07-02 07:54:07 +0300

    Solved question 2

--- OOP01/Program.cs
+++ OOP01/Program.cs
@@ -17,6 +17,21 @@
             Sunday
         }
         #endregion
+
+        #region Question2
+        //Define a struct "Person" with properties "Name" and "Age". Create an array of three "Person" objects and populate it with data.
+        //Then, write a C# program to display the details of all the persons in the array.
+        public struct Person
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public Person(string name, int age)
+            {
+                this.Name = name;
+                this.Age = age;
+            }
+        }
+        #endregion
         public static void Main(string[] args)
         {
             #region Question1
@@ -25,6 +40,19 @@
             foreach (WeekDays day in Enum.GetValues(typeof(WeekDays)))
             {
                 Console.WriteLine(day);
+            }
+            #endregion
+
+            #region Question2
+            //Define a struct "Person" with properties "Name" and "Age". Create an array of three "Person" objects and populate it with data.
+            //Then, write a C# program to display the details of all the persons in the array.
+            Person[] people = new Person[3];
+            people[0] = new Person("Zeyad", 25);
+            people[1] = new Person("Ahmed", 20);
+            people[2] = new Person("Jack", 21);
+            foreach (var person in people)
+            {
+                Console.WriteLine($"Name: {person.Name}, Age: {person.Age}");
             }
             #endregion
         }
